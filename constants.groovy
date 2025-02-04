415be4d3eb18ad3ecf44c17fee3bb821b95d0292
Chandankumar AC <chandanchowdegowda@gmail.com>
2025-02-04 13:01:11 +0530

    Update constants.groovy

--- constants.groovy
+++ constants.groovy
@@ -4,7 +4,7 @@ import groovy.transform.Field
 @Field String AWS_ACCOUNT_ID = '920373030315'
 @Field String AWS_DEFAULT_REGION = 'us-east-1'
 @Field String IMAGE_REPO_NAME = 'sample-webapp'
-@Field String GIT_REPO_URL = 'https://github.com/girishballur123/Maven-project.git'
+@Field String GIT_REPO_URL = 'https://github.com/Chandan-2507/sample-webapp.git'
 
 // Jenkins Credential IDs
 @Field String GIT_CREDENTIALS_ID = 'Jenkinslatest'
